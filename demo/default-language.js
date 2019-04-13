2cf20c56bf8c50c633f044a39c06e73232c88b26
Felix Zapata <fzberlinches@gmail.com>
2019-04-13 11:48:22 +0200

    feat(demo): add default-language litElement

---
+++ demo/default-language.js
@@ -0,0 +1,42 @@
+import { LitElement } from 'lit-element';
+import '../lit-element-datepicker.js';
+
+class DefaultLanguage extends LitElement {
+  static get properties() {
+    return {
+      datePickerValue: {
+        type: String,
+        value: ''
+      }
+    };
+  }
+  render() {
+    return html`
+      <div role="application">
+        <label for="date">Date:</label>
+        <input id="date" type="text" value="${dateFormat(datePickerValue, 'L')}" />
+        <button type="button">Select Date...</button>
+      </div>
+      <lit-element-datepicker id="datePicker" date="${datePickerValue}"></lit-element-datepicker> 
+    `;
+  }
+  dateFormat(date, format) {
+    if(date !== '') {
+      return window.moment(date).format(format);
+    }
+    return '';
+  }
+  _openCalendar() {
+    this.shadowRoot.querySelector('#datePicker').showDlg();
+  }
+  firstUpdated() {
+    this.openCalendarHandler = this._openCalendar.bind(this);
+    this.shadowRoot.querySelector('button').addEventListener('click', this.openCalendarHandler);
+  }
+  disconnectedCallback() {
+    super.disconnectedCallback();
+    this.openCalendarHandler = this._openCalendar.bind(this);
+    this.shadowRoot.querySelector('button').addEventListener('click', this.openCalendarHandler);
+  }
+}
+customElements.define('default-language', DefaultLanguage);
