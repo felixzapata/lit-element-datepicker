1d219396e4af10cb03925aa3969e07dec6e44874
Felix Zapata <fzberlinches@gmail.com>
2023-10-25 16:30:43 +0200

    test: add more tests

--- test/lit-element-datepicker.test.ts
+++ test/lit-element-datepicker.test.ts
@@ -3,20 +3,13 @@ import { beforeEach, describe, expect, it, afterEach, vi } from 'vitest';
 import { fixture, html } from '@open-wc/testing';
 
 describe('lit-element-datepicker', () => {
-  let element: LitElementDatepicker;
-
   beforeEach(() => {
     vi.useFakeTimers();
     vi.setSystemTime(new Date(2019, 0, 24));
-    /* element = document.createElement(
-      'lit-element-datepicker'
-    ) as LitElementDatepicker;
-    document.body.appendChild(element); */
   });
 
   afterEach(() => {
     vi.useRealTimers();
-    // document.body.removeChild(element);
   });
 
   it('is defined', () => {
@@ -24,8 +17,7 @@ describe('lit-element-datepicker', () => {
     expect(el).to.be.instanceOf(LitElementDatepicker);
   });
 
-  it('should open a calendar with current date on January 24, 2019', async () => {
-    // element.opened = true;
+  it('should set a calendar with current date on January 24, 2019', async () => {
     var element = await fixture(html`
       <lit-element-datepicker id="datePicker"></lit-element-datepicker>
     `);
@@ -35,8 +27,60 @@ describe('lit-element-datepicker', () => {
     expect(datePicker?.querySelector('#month')?.firstChild?.nodeValue).to.equal('January 2019');
     expect(table?.querySelectorAll('tbody .empty').length).to.equal(4);
     expect(table?.querySelectorAll('tbody td[id]').length).to.equal(31);
-    expect(
-      table?.querySelector('#day24')?.classList.contains('today')
-    ).to.equal(true);
+    expect(table?.querySelector('#day24')?.classList.contains('today')).to.equal(true);
+  });
+  it('should set a calendar with current date on 24 enero, 2019', async () => {
+    var element = await fixture(html`
+      <lit-element-datepicker id="datePicker" locale="es"></lit-element-datepicker>
+    `);
+    const datePicker = element.shadowRoot?.querySelector('#dp1');
+    const table = datePicker?.querySelector('table');
+    expect(table?.querySelectorAll('thead th').length).to.equal(7);
+    expect(datePicker?.querySelector('#month')?.firstChild?.nodeValue).to.equal('enero 2019');
+    expect(table?.querySelectorAll('tbody .empty').length).to.equal(4);
+    expect(table?.querySelectorAll('tbody td[id]').length).to.equal(31);
+    expect(table?.querySelector('#day24')?.classList.contains('today')).to.equal(true);
+  });
+  it('should set a calendar with an init date of February 08, 2013', async () => {
+    var element = await fixture(html`
+      <lit-element-datepicker id="datePicker" init-date="2013-02-08"></lit-element-datepicker>
+    `);
+    const datePicker = element.shadowRoot?.querySelector('#dp1');
+    const table = datePicker?.querySelector('table');
+    expect(table?.querySelectorAll('thead th').length).to.equal(7);
+    expect(datePicker?.querySelector('#month')?.firstChild?.nodeValue).to.equal('February 2013');
+    expect(table?.querySelectorAll('tbody .empty').length).to.equal(7);
+    expect(table?.querySelectorAll('tbody td[id]').length).to.equal(28);
+    expect(table?.querySelector('#day24')?.classList.contains('today')).to.equal(true);
+  });
+  it('should set a calendar with a min date of February 08, 2013 and max date of April 08, 2013', async () => {
+    var element = await fixture(html`
+      <lit-element-datepicker id="datePicker" init-date="2013-03-08" min-date="2013-02-11" max-date="2013-04-08"></lit-element-datepicker>
+    `);
+    const datePicker = element.shadowRoot?.querySelector('#dp1');
+    const table = datePicker?.querySelector('table');
+    expect(table?.querySelectorAll('thead th').length).to.equal(7);
+    expect(datePicker?.querySelector('#month')?.firstChild?.nodeValue).to.equal('March 2013');
+    expect(table?.querySelectorAll('tbody .empty').length).to.equal(4);
+    expect(table?.querySelectorAll('tbody .disabled').length).to.equal(11);
+    expect(table?.querySelectorAll('tbody td[id]').length).to.equal(31);
+    expect(table?.querySelector('#day24')?.classList.contains('today')).to.equal(true);
+    expect(datePicker?.querySelector('#bn_prev')?.getAttribute('aria-hidden')).to.equal('true');
+    expect(datePicker?.querySelector('#bn_next')?.getAttribute('aria-hidden')).to.equal('false');
+  });
+  it('should set a calendar with a min date of February 08, 2013 and max date of February 29, 2013', async () => {
+    var element = await fixture(html`
+      <lit-element-datepicker id="datePicker" init-date="2013-03-08" min-date="2013-02-11" max-date="2013-02-29"></lit-element-datepicker>
+    `);
+    const datePicker = element.shadowRoot?.querySelector('#dp1');
+    const table = datePicker?.querySelector('table');
+    expect(table?.querySelectorAll('thead th').length).to.equal(7);
+    expect(datePicker?.querySelector('#month')?.firstChild?.nodeValue).to.equal('March 2013');
+    expect(table?.querySelectorAll('tbody .empty').length).to.equal(4);
+    expect(table?.querySelectorAll('tbody .disabled').length).to.equal(11);
+    expect(table?.querySelectorAll('tbody td[id]').length).to.equal(31);
+    expect(table?.querySelector('#day24')?.classList.contains('today')).to.equal(true);
+    expect(datePicker?.querySelector('#bn_prev')?.getAttribute('aria-hidden')).to.equal('true');
+    expect(datePicker?.querySelector('#bn_next')?.getAttribute('aria-hidden')).to.equal('false');
   });
 });
